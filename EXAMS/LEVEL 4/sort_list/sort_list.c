f2aa08a230e8405d2622437402aa38658d7fbe44
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-01-26 15:53:35 +0100

    1337

---
+++ EXAMS/LEVEL 4/sort_list/sort_list.c
@@ -0,0 +1,29 @@
+typedef struct s_list t_list;
+
+struct s_list
+{
+	int     data;
+	t_list  *next;
+};
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	int overflow;
+	t_list *tmp;
+
+	tmp = lst;
+	while (lst->next)
+	{
+		if (cmp(lst->data, lst->next->data) == 0)
+		{
+			overflow = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = overflow;
+			lst = tmp;
+		}
+		else
+			lst = lst->next;
+	}
+	lst = tmp;
+	return lst;
+}
