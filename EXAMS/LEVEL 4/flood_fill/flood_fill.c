35d81758dc9acfbe91bd74c313548e083e0ff0e2
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-01-26 15:48:39 +0100

    1337

---
+++ EXAMS/LEVEL 4/flood_fill/flood_fill.c
@@ -0,0 +1,38 @@
+typedef struct  s_point
+{
+int           x;
+int           y;
+}               t_point;
+
+void  flood_fill(char **tab, t_point size, t_point begin)
+{
+	char c;
+	t_point p;
+
+	c = tab[begin.y][begin.x];
+	tab[begin.y][begin.x] = 'F';
+	if (begin.y > 0 && tab[begin.y - 1][begin.x] == c)
+	{
+		p.x = begin.x;
+		p.y = begin.y - 1;
+		flood_fill(tab, size, p);
+	}
+	if (begin.y < (size.y - 1) && tab[begin.y + 1][begin.x] == c)
+	{
+		p.x = begin.x;
+		p.y = begin.y + 1;
+		flood_fill(tab, size, p);
+	}
+	if (begin.x < (size.x - 1) && tab[begin.y][begin.x + 1] == c)
+	{
+		p.x = begin.x + 1;
+		p.y = begin.y;
+		flood_fill(tab, size, p);
+	}
+	if (begin.x > 0 && tab[begin.y][begin.x - 1] == c)
+	{
+		p.x = begin.x - 1;
+		p.y = begin.y;
+		flood_fill(tab, size, p);
+	}
+}
